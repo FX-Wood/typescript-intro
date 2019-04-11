5d3793229d84c22407a114244e695f05a1b2ce2e
FX-Wood <fxwood2@gmail.com>
2019-04-11 14:18:56 -0700

    Adding basic types

--- intro.ts
+++ intro.ts
@@ -1,1 +1,39 @@
-var hello: string = "Hello world"
+// strings
+var hello: string = "Hello world";
+var barf: string = "Yak, Bark";
+var barfLiteral: string = `literally, ${barf}`;
+
+// number
+var myInt: number = 3;
+var myFloat: number = 6.4;
+var myHex: number = 0xf00d;
+
+// booleans
+var myBool: boolean
+myBool = true;
+
+// any
+var myAny = null;
+myAny = 'here i am';
+myAny = 2349;
+
+var myStrings: string[] = ['asd', 'dfg']
+var moreStrings: Array<string> = ['asd','dfg']
+
+var twoDArray: Array<number[]> = [[2,3], [3,3]]
+
+// functions
+function myFunc(arg: string, arg2: number): string[] {
+    return Array(arg2).fill(arg)
+}
+
+const myArrowFunc = (arg:string, arg2: number): string => {
+    // logic
+    return ''
+}
+
+function errorLogger(items: string[]): void {
+    items.forEach( (item: string) => {
+        console.log(item)
+    })
+}
